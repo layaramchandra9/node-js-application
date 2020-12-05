c43ab150d081ffac37482738824ef2f7dfe7026b
layaramchandra9 <67433365+layaramchandra9@users.noreply.github.com>
2020-12-05 16:52:34 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.22.169.159:9000/',
+  serverUrl: 'http://54.92.189.69:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application - CICD',
 	    'sonar.projectName': 'Node JS Application - Sample',
